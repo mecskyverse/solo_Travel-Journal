f20d34ad460130f9650bc09bf13090f48ca43d28
Aakash <vermaaakash1000@gmail.com>
2022-08-25 10:56:44 +0530

    Card component props added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import './App.css'
 import Navbar from './components/Navbar'
+import Card from './components/Card'
+import data from './data'
+
 function App() {
+  const cards = data.map(element => {
+    return (
+      <>
+        <Card
+          {...element}
 
+        />
 
+      </>
+    )
+  })
+  console.log(cards)
   return (
     <div className="container">
       <div className="App">
         <Navbar />
+        <section className='card-list'>
+          {cards}
+        </section>
       </div>
     </div>
   )
