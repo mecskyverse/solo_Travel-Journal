9215567a16e7e0d6cd80286642cec0657fdd06f1
Aakash <vermaaakash1000@gmail.com>
2022-08-25 10:15:48 +0530

    Navbar created

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,11 @@
 import React from "react";
-
+import globe from "../images/globe.png"
 export default function Navbar() {
     return (
-        <div>Hello</div>
+        <div className="Nav">
+            <img src={globe} alt="globe Image" className="globe" />
+            <p>My Travel Journal</p>
+        </div>
     )
 
 }
