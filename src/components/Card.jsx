4e615f6e888fd34bac043609e544772bca7f54cd
Aakash <vermaaakash1000@gmail.com>
2022-08-26 16:09:27 +0530

    New props added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,11 @@ export default function Card(props) {
     return (
         <div className="card-container">
             <img src={props.imageUrl} className="card-image" />
-            <div>
-
+            <div className="info">
+                <div>{props.location} <a className="location-link" href={props.googleMapsUrl} target="_blank">View on Google Maps</a></div>
+                <div className="info-title">{props.title}</div>
+                <div className="info-date">{props.startDate}&nbsp;-&nbsp;{props.endDate}</div>
+                <p className="info-description">{props.description}</p>
             </div>
         </div>
     )
